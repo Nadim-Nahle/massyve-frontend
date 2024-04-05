d13e47faef0f2e8ad453cc8dff30efc6e96432de
nadim nahle <nahlenadim@gmail.com>
2024-04-05 12:37:14 +0300

    Update login page

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,35 +1,41 @@
 "use client";
-import { useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
+  const [message, setMessage] = useState(null); // State to manage error or success message
+  const router = useRouter()
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     try {
       const response = await axios.post('https://massyve-six.vercel.app/api/v1/users/login', {
         email,
         password
       });
-
+      router.push('/welcome')
       console.log('Login successful', response.data);
-      // Redirect or perform any other actions after successful login
+      // Set success message
+      setMessage('Login successful!');
     } catch (error) {
       console.error('Login error', error.response.data);
-      setError('Invalid email or password');
+      // Set error message
+      setMessage('Invalid email or password');
     }
   };
 
   return (
     <div className="container">
+      {/* Display error or success message */}
+      {message && <div className={message.includes('success') ? 'success-message' : 'error'}>{message}</div>}
+
       <form className="form" onSubmit={handleSubmit}>
         <h1>Login</h1>
-        {error && <div className="error">{error}</div>}
         <div className="form-group">
           <label>Email:</label>
           <input className="form-control" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
@@ -41,10 +47,11 @@ const LoginPage = () => {
         <button className="btn btn-primary" type="submit">Login</button>
       </form>
       <div className="register-link">
-        <p>New here? <Link href="/register">Register now</Link></p>
+        <p>New here? <Link href="/signup">Signup now</Link></p>
       </div>
     </div>
   );
 };
+
 
 export default LoginPage;
