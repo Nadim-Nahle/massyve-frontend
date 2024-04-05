13eab6aefb03e71ea90bec7f17d2681e058ba212
nadim nahle <nahlenadim@gmail.com>
2024-04-05 10:43:27 +0300

    Add logi n API

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,35 +1,47 @@
 "use client";
 import { useState } from 'react';
-import Link from 'next/link'; // Import Link from Next.js
+import axios from 'axios';
+import Link from 'next/link';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Handle login logic here
-    console.log('Login form submitted');
+    
+    try {
+      const response = await axios.post('https://massyve-six.vercel.app/api/v1/users/login', {
+        email,
+        password
+      });
+
+      console.log('Login successful', response.data);
+      // Redirect or perform any other actions after successful login
+    } catch (error) {
+      console.error('Login error', error.response.data);
+      setError('Invalid email or password');
+    }
   };
 
   return (
     <div className="container">
-      <div className="form-container">
-        <form className="form" onSubmit={handleSubmit}>
-          <h1>Login</h1>
-          <div className="form-group">
-            <label>Email:</label>
-            <input className="form-control" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-          </div>
-          <div className="form-group">
-            <label>Password:</label>
-            <input className="form-control" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          </div>
-          <button className="btn btn-primary" type="submit">Login</button>
-        </form>
-      </div>
+      <form className="form" onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        {error && <div className="error">{error}</div>}
+        <div className="form-group">
+          <label>Email:</label>
+          <input className="form-control" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input className="form-control" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button className="btn btn-primary" type="submit">Login</button>
+      </form>
       <div className="register-link">
-        <p>New here? <Link href="/signup">Signup now</Link></p>
+        <p>New here? <Link href="/register">Register now</Link></p>
       </div>
     </div>
   );
