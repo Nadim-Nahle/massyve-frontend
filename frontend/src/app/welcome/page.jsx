c2d45d91e9a6d1efab0665d82bb11d437a3cffec
nadim nahle <nahlenadim@gmail.com>
2024-04-05 13:16:05 +0300

    Update welcome page

--- frontend/src/app/welcome/page.jsx
+++ frontend/src/app/welcome/page.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
+import { useEffect } from "react";
 
 function WelcomePage() {
+  const [userData, setUserData] = useState('');
+  useEffect(() => {
+    // Perform localStorage action
+    const data = JSON.parse(localStorage.getItem('userData'));
+    setUserData(data);
+    console.log('test',userData)
+  }, [])
   return (
-    <div>WelcomePage</div>
-  )
+    <div className="container-1">
+      <div className="heading">Welcome Page</div>
+      <div className="user-info">
+        <div>
+          <label>Name:</label> {userData.name}
+        </div>
+        <div>
+          <label>Email:</label> {userData.email}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default WelcomePage
+export default WelcomePage;
